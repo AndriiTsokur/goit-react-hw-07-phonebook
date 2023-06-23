f77468c9d6910f0d00573866842db01034119b2e
Andrii Tsokur <tsokur@gmail.com>
2023-06-23 17:56:25 +0300

    Adds completed steps 3-4

--- src/components/Phonebook/ContactsList.jsx
+++ src/components/Phonebook/ContactsList.jsx
@@ -1,22 +1,49 @@
 import React from 'react';
 import css from './ContactsList.module.css';
 
-export function ContactsList({ data }) {
-	return (
-		<div className={css.contacts__container}>
-			<h1 className={css.contacts__title}>Contacts</h1>
+export class ContactsList extends React.Component {
+	state = {
+		filter: '',
+	};
 
-			{data.length === 0 ? (
-				<p className={css.contacts__empty}>Contacts list is empty</p>
-			) : (
-				<ul className={css.contacts__list}>
-					{data.map(item => (
-						<li key={item.id} className={css.contacts__item}>
-							{item.name}: {item.number}
-						</li>
-					))}
-				</ul>
-			)}
-		</div>
-	);
+	handleInput = e => {
+		this.setState({ filter: e.target.value });
+	};
+
+	render() {
+		return (
+			<div className={css.contacts__container}>
+				<h1 className={css.contacts__title}>Contacts</h1>
+				{this.props.data.length === 0 ? (
+					<p className={css.contacts__empty}>Contacts list is empty</p>
+				) : (
+					<>
+						<label className={css.contacts__label}>
+							Find contacts by name:
+							<input
+								onChange={this.handleInput}
+								value={this.state.filter}
+								className={css.contacts__input}
+								type="text"
+								name="filter"
+								required
+							/>
+						</label>
+
+						<ul className={css.contacts__list}>
+							{this.props.data
+								.filter(({ name }) =>
+									name.toLowerCase().includes(this.state.filter.toLowerCase())
+								)
+								.map(item => (
+									<li key={item.id} className={css.contacts__item}>
+										{item.name}: {item.number}
+									</li>
+								))}
+						</ul>
+					</>
+				)}
+			</div>
+		);
+	}
 }
